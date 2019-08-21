6f1b3853790bb12ae2c264a01909aa9ff4133fcc
ShaDuv <Shawnabduvall@gmail.com>
2019-08-21 15:46:37 -0700

    Add seeding
    Co-Authored-By: BenB-C <bcauchon@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'factory_bot_rails'
+
+class Seed
+  def self.begin
+    Place.destroy_all
+    seed = Seed.new
+    seed.generate_places_with_reviews
+  end
+
+  def generate_places_with_reviews
+    100.times do
+      FactoryBot.create(:place_with_reviews)
+    end
+  end
+end
+
+Seed.begin
